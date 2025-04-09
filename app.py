d04a3cb89e668e56cc915fdf375cac6571cae1c0
Miłosz <milsli69.gmail.com>
2025-04-09 19:03:51 +0200

    ostateczna wersja aplikacji

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
 import json
 import streamlit as st
 import pandas as pd  # type: ignore
-from pycaret.clustering import load_model, predict_model  # type: ignore
+from pycaret.clustering import predict_model, setup, create_model, assign_model
 import plotly.express as px  # type: ignore
 
-MODEL_NAME = 'welcome_survey_clustering_pipeline_v1'
-DATA = 'welcome_survey_simple_v1.csv'
-CLUSTER_NAMES_AND_DESCRIPTIONS = 'welcome_survey_cluster_names_and_descriptions_v1.json'
-
+DATA = 'welcome_survey_simple_v2.csv'
+CLUSTER_NAMES_AND_DESCRIPTIONS = 'cluster_names_and_descriptions.json'
 
 @st.cache_data
-def get_model():
-    return load_model(MODEL_NAME)
+def prepare_model():
+    all_df = pd.read_csv(DATA, sep=';')
+    setup(all_df, session_id=901)
+    kmeans = create_model('kmeans', num_clusters=8)
+    model = assign_model(kmeans)
+    return kmeans, model
 
 @st.cache_data
 def get_cluster_names_and_descriptions():
     with open(CLUSTER_NAMES_AND_DESCRIPTIONS, "r", encoding='utf-8') as f:
         return json.loads(f.read())
 
-@st.cache_data
-def get_all_participants():
-    model = get_model()
-    all_df = pd.read_csv(DATA, sep=';')
-    df_with_clusters = predict_model(model, data=all_df)
-
-    return df_with_clusters
-
 with st.sidebar:
     st.header("Powiedz nam coś o sobie")
     st.markdown("Pomożemy Ci znaleźć osoby, które mają podobne zainteresowania")
-    age = st.selectbox("Wiek", ['<18', '25-34', '45-54', '35-44', '18-24', '>=65', '55-64', 'unknown'])
+    age = st.selectbox("Wiek", ['<18',  '18-24', '25-34', '35-44', '45-54', '55-64','>=65', 'unknown'])
     edu_level = st.selectbox("Wykształcenie", ['Podstawowe', 'Średnie', 'Wyższe'])
     fav_animals = st.selectbox("Ulubione zwierzęta", ['Brak ulubionych', 'Psy', 'Koty', 'Inne', 'Koty i Psy'])
     fav_place = st.selectbox("Ulubione miejsce", ['Nad wodą', 'W lesie', 'W górach', 'Inne'])
@@ -46,16 +40,17 @@ with st.sidebar:
     ])
 
 
-model = get_model()
-all_df = get_all_participants()
+kmeans, assigned_model_df = prepare_model()
 cluster_names_and_descriptions = get_cluster_names_and_descriptions()
 
-predicted_cluster_id = predict_model(model, data=person_df)["Cluster"].values[0]
+
+
+predicted_cluster_id = predict_model(kmeans, data=person_df)["Cluster"].values[0]
 predicted_cluster_data = cluster_names_and_descriptions[predicted_cluster_id]
 
 st.header(f"Najbliżej Ci do grupy {predicted_cluster_data['name']}")
 st.markdown(predicted_cluster_data['description'])
-same_cluster_df = all_df[all_df["Cluster"] == predicted_cluster_id]
+same_cluster_df = assigned_model_df[assigned_model_df["Cluster"] == predicted_cluster_id]
 st.metric("Liczba twoich znajomych", len(same_cluster_df))
 
 st.header("Osoby z grupy")
